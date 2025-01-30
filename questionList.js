291652b73f516c12fbc26ad1e36e84ace6a32288
Suraj Gharpankar <surajgharpankar28@gmail.com>
2025-01-30 13:58:01 +0530

    question 4 - initiated

--- questionList.js
+++ questionList.js
@@ -2,6 +2,7 @@ const questionList = {
   1: "GitHub user search component",
   2: "Todo List Application",
   3: "Password Strength Checker",
+  4: "Infinite Scroll Component",
 };
 
 export default questionList;
---
+++ src/pages/Question_4/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="min-h-[calc(100vh-6rem)] flex items-center justify-center bg-gray-100 px-4">
+      Question 4
+    </div>
+  );
+}
+
+export default App;
