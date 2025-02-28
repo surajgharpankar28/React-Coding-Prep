1a8ce42de206e39cd0cdecb1dfb7bce236b2fc82
Suraj Gharpankar <surajgharpankar28@gmail.com>
2025-02-28 15:52:11 +0530

    Improved Old UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import questionList from "../questionList";
 import { Link } from "react-router-dom";
-import { Github } from "lucide-react";
+import { FaGithub, FaGlobe } from "react-icons/fa6";
 
 const QUESTIONS_PER_PAGE = 7;
 
@@ -18,9 +18,14 @@ export default function QuestionList() {
 
   return (
     <div className="min-h-[calc(100vh-6rem)] w-full bg-gradient-to-r from-blue-500 to-purple-600 flex flex-col items-center justify-center text-white p-4 sm:p-24">
-      <h1 className="text-3xl sm:text-5xl font-bold mb-6 sm:mb-8 text-center animate-fade-in">
-        Level Up Your React Skills!
-      </h1>
+      <div className="text-center mb-8">
+        <h1 className="text-3xl sm:text-5xl font-bold text-white">
+          🚀 Level Up Your React Skills! 🚀
+        </h1>
+        <p className="text-sm sm:text-lg font-semibold text-gray-200 mt-2">
+          My 28 Days React Coding Challenge
+        </p>
+      </div>
       <div className="bg-white/10 rounded-2xl shadow-2xl p-6 sm:p-8 max-w-4xl w-full border border-white/10">
         <ul className="space-y-3">
           {paginatedQuestions.map(([id, title]) => (
@@ -37,25 +42,37 @@ export default function QuestionList() {
               >
                 {id} - {title}
               </Link>
-
-              <Link
-                target="_blank"
-                to={`https://github.com/surajgharpankar28/React-Coding-Prep/tree/main/src/pages/Question_${id}`}
-                className="flex items-center gap-2 hover:text-black"
-              >
-                <span>Check on GitHub</span>
-                <Github />
-              </Link>
+              <div className="flex items-center gap-2 sm:gap-4">
+                <Link
+                  target="_blank"
+                  to={`/question-${id}-${title
+                    .toLowerCase()
+                    .replace(/\s+/g, "-")}`}
+                  className="flex items-center gap-2  px-4 py-2 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition"
+                >
+                  Explore Live <FaGlobe size={22} />
+                </Link>
+                <Link
+                  target="_blank"
+                  to={`https://github.com/surajgharpankar28/React-Coding-Prep/tree/main/src/pages/Question_${id}`}
+                  className="flex items-center gap-2 hover:text-black"
+                >
+                  <span>Check on GitHub</span>
+                  <FaGithub size={24} />
+                </Link>
+              </div>
             </div>
           ))}
         </ul>
       </div>
       {Object.keys(questionList).length > QUESTIONS_PER_PAGE && (
-        <div className="flex justify-center space-x-4 mt-6">
+        <div className="flex justify-center items-center space-x-4 mt-6">
           <button
             onClick={() => setCurrentPage((prev) => Math.max(prev - 1, 1))}
             disabled={currentPage === 1}
-            className="px-4 py-2 bg-white/20 text-white rounded-lg shadow-md hover:bg-white/30 disabled:opacity-50"
+            className={`px-4 py-2 ${
+              currentPage === 1 ? "cursor-not-allowed" : "cursor-pointer"
+            } bg-white/20 text-white rounded-lg shadow-md hover:bg-white/30 disabled:opacity-50`}
           >
             Previous
           </button>
@@ -67,7 +84,11 @@ export default function QuestionList() {
               setCurrentPage((prev) => Math.min(prev + 1, totalPages))
             }
             disabled={currentPage === totalPages}
-            className="px-4 py-2 bg-white/20 text-white rounded-lg shadow-md hover:bg-white/30 disabled:opacity-50"
+            className={`px-4 py-2 ${
+              currentPage === totalPages
+                ? "cursor-not-allowed"
+                : "cursor-pointer"
+            }    bg-white/20 text-white rounded-lg shadow-md hover:bg-white/30 disabled:opacity-50`}
           >
             Next
           </button>
